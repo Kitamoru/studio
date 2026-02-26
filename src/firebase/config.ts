516cac5f9e8db63269a8b247752f6e893e917504
Вадим Иванов <kkitamoru@gmail.com>
2026-02-26 12:27:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9287918196-37b30",
+  "appId": "1:407984508781:web:c74bd53364ad8f88b59464",
+  "apiKey": "AIzaSyCWmxIUlNyCXV--G4yZgz_-KpYoyroasmw",
+  "authDomain": "studio-9287918196-37b30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "407984508781"
+};
