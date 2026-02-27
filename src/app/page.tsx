01a6b6f2544a70543a15d9e7d7fe9052ba8eed49
Вадим Иванов <kkitamoru@gmail.com>
2026-02-27 19:56:17 +0000

    Поставь ограничение max-w-[800px]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import GameCanvas from '@/components/GameCanvas';
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full flex flex-col bg-[#050406]">
-      <GameCanvas />
+    <main className="min-h-screen w-full flex justify-center bg-[#050406]">
+      <div className="w-full max-w-[800px] min-h-screen">
+        <GameCanvas />
+      </div>
     </main>
   );
 }
