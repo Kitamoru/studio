bbbfc352917070aae9172ff57dcf205b77cc76fe
Вадим Иванов <kkitamoru@gmail.com>
2026-02-28 07:53:02 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
         <script src="https://telegram.org/js/telegram-web-app.js" async></script>
       </head>
-      <body className="font-body antialiased bg-[#0f0d12] text-white">
+      <body className="font-body antialiased bg-[#0f0d12] text-white" suppressHydrationWarning>
         <FirebaseClientProvider>
           <DndProvider>
             {children}
